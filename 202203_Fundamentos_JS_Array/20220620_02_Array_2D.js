d4c065aa2c1352c243b3064520e7ec5f552f4a1e
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-03-20 10:15:32 -0300

    Criação de arquivo com a lógica da aula sobre arrays 2D

---
+++ 202203_Fundamentos_JS_Array/20220620_02_Array_2D.js
@@ -0,0 +1,8 @@
+//Array de duas dimensões
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const mediaAlunos = [10, 7, 9, 6];
+//Declaração de variável e atribuição de duas arrays
+let listaNotasAlunos = [alunos, mediaAlunos];
+//Exibição formatada dos elementos da array lsitaNotasAlunos
+console.log(`${listaNotasAlunos[0][0]} sua média é
+${listaNotasAlunos[1] [0]}`)
