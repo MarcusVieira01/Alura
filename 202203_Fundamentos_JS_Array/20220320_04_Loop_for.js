e17dee922040ddeb70632e615357cb08c67d5778
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-03-20 11:10:22 -0300

    Criação de arquivo com a lógica da aula sobre loop for com arrays

---
+++ 202203_Fundamentos_JS_Array/20220320_04_Loop_for.js
@@ -0,0 +1,10 @@
+//Loop for com arrays
+//Declaração de variável e atribuição dos valores de uma lista
+const numeros = [100, 200, 300, 400, 500, 600];
+//Loop for que exibirá cada elemento da lista, sendoo índice o 
+//valor da variável de controle i
+for(let i = 0; i < numeros.length; i++){
+    //Exibição via console.log(par) do valor da variável i e do
+    //valor do índice i da lista números
+    console.log(i, numeros[i]);
+}
