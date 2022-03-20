2173de691095aa34db84827b59cfd399f214fb69
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-03-20 09:58:49 -0300

    Criação de arquivo com a lógica da aula de concatenação de duas arrays

---
+++ 202203_Fundamentos_JS_Array/20220320_01_Concatenando.js
@@ -0,0 +1,9 @@
+//Concatenando Arrays
+//Declaração das arrays
+const salaPython = ['Marcus', 'Melissa', 'Helena', 'Rodrigo'];
+const salaJS = ['Ju', 'Leo', 'Raquel'];
+//Declaração de variável que receberá o retorno do método
+//.concat(par)
+let salasUnidas = salaPython.concat(salaJS);
+//Exibição do valor da variável salasUnidas
+console.log(salasUnidas);
