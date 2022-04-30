ec0a6fe41b3fc5d2b51813da353e7640dcf71df4
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-04-30 18:38:03 -0300

    Alteração da sequência de elementos para melhor visualização do algoritmo aplicado no arquivo 20220402_02_Separacao_pivo.js

--- 202203_Algoritmos_JS_Ordenação_02/20220402_01_Array.js
+++ 202203_Algoritmos_JS_Ordenação_02/20220402_01_Array.js
@@ -6,9 +6,9 @@ const listaLivros = [
     { titulo: "Java", preco: 30 },
     { titulo: "Go", preco: 45 },
     { titulo: "Elixir", preco: 50 },
+    { titulo: "Ruby", preco: 28 },
     { titulo: "Python", preco: 20 },
     { titulo: "Rust", preco: 22 },
-    { titulo: "Ruby", preco: 28 },
     { titulo: "C#", preco: 33 },
     { titulo: "C++", preco: 35 },
     { titulo: "Scala", preco: 40 }
