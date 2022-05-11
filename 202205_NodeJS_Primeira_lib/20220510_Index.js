d53f271317923a790db91fa03bcc536f94ff4889
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-05-11 12:01:55 -0300

    Atualização do código referente à aula de tratamento de erros

--- 202205_NodeJS_Primeira_lib/20220510_Index.js
+++ 202205_NodeJS_Primeira_lib/20220510_Index.js
@@ -2,11 +2,20 @@
 //Declaração de variáveis e atribuição do valor de retorno do método require(args) que fará o carregamento dos módulos externos
 const chalk = require('chalk');
 const fs = require('fs');//Módulo File Sistem
+//
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'Faiô'));
+}
+
 //Declaração de função que fará a exibição do conteúdo de um arquivo usando métoo da biblioteca FS, usando o método chalk.green(par) para colorir o texto
 function pegaArquivo(caminhoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoArquivo, encoding, (_, texto) => {console.log(chalk.green(texto))
+    fs.readFile(caminhoArquivo, encoding, (erro, texto) => {
+        if(erro){
+            trataErro(erro);
+        }
+        console.log(chalk.green(texto));
     })
 }
 //Evoca a função pegaArquivo(path)
-pegaArquivo('./texto.md')
+pegaArquivo('./texto.md');
