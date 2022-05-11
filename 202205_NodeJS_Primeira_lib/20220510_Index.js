01902d98646be15f5eb840e972625acdf111b2ce
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-05-11 11:42:41 -0300

    Alteração do arquivo para a lógica dada na aula sobre a biblioteca FS

--- 202205_NodeJS_Primeira_lib/20220510_Index.js
+++ 202205_NodeJS_Primeira_lib/20220510_Index.js
@@ -1,5 +1,12 @@
 //
-//
+//Declaração de variáveis e atribuição do valor de retorno do método require(args) que fará o carregamento dos módulos externos
 const chalk = require('chalk');
-//
-console.log(chalk.blue('Olá mundo!'));
+const fs = require('fs');//Módulo File Sistem
+//Declaração de função que fará a exibição do conteúdo de um arquivo usando métoo da biblioteca FS, usando o método chalk.green(par) para colorir o texto
+function pegaArquivo(caminhoArquivo){
+    const encoding = 'utf-8';
+    fs.readFile(caminhoArquivo, encoding, (_, texto) => {console.log(chalk.green(texto))
+    })
+}
+//Evoca a função pegaArquivo(path)
+pegaArquivo('./texto.md')
