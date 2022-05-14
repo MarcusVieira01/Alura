b7f404da6c00d2c9577b570b8e9cf1355cbde205
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-05-13 23:09:16 -0300

    Atualização de arquivos referente a segunda aula de montando o objeto

--- 202205_NodeJS_Primeira_lib/cli.js
+++ 202205_NodeJS_Primeira_lib/cli.js
@@ -3,8 +3,10 @@
 const chalk = require('chalk');
 const pegaArquivo = require('./20220510_Index');
 const validaURL = require('./20220513_http_valid');
+
 //Declaração de variável que recebera o retorno do método process.argv, sendo um array com os caminhos que o arquivo percorre e o valor do input pelo usuário
 const caminho = process.argv;
+
 // Declaração de função assincrona que fará a chamada da função pegaArquivo passando um caminho como parâmetro, onde essa função é uma promessa
 async function processaTexto(caminhoArquivo){
     //Declaração de variável e atribuição do retorno do método pegaArquivo(args), que se torna uma promessa por ter a keyword await usada
@@ -14,9 +16,10 @@ async function processaTexto(caminhoArquivo){
         //Caso true, será evocada a função validaURL(args) e exibido o seu retorno de forma formatada e colorida com chalk
         console.log(chalk.yellow('links validados'), await validaURL(resultado));
     }else{
-        //Caso true, será exobido o valor da variável resultado, de forma formatada e colorida com chalk
+        //Caso false, será exibido o valor da variável resultado de forma formatada e colorida com chalk
         console.log(chalk.yellow('Lista de links'), resultado);
     }
 }
+
 //Evocação da função processaTexto, com parâmetro sendo o valor da variável caminho
 processaTexto(caminho);
