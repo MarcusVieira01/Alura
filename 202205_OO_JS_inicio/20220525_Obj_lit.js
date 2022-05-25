b9dcd68bc6fcb2313b331bc173e59f8149280df7
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-05-25 19:58:43 -0300

    Criação de arquivo para uso nas aulas posteriores, com a lógica da aula sobre herança de protótipo

--- 202205_OO_JS_inicio/20220525_Obj_lit.js
+++ 202205_OO_JS_inicio/20220525_Obj_lit.js
@@ -1,25 +1,27 @@
-//Criação de um objeto literal com as propriedades definidas
+//Criação de objetos literais com as propriedades definidas
 const user = {
     nome: "Juliana",
     email: 'j@j.com',
     nascimento: '2021/01/02',
-    role:'admin',
+    role:'estudante',
     ativo: true,
     exibirInfos: function(){
         console.log(this.nome,this.email);
     }
 };
-//Chamada de método do objeto user
-user.exibirInfos();
-//Declaração de variável e atribuição do retorno da função exibirInfos, sem () pois não será executada a função e sim apenas atribuído retorno, do objeto user
-const exibir = user.exibirInfos;
-//Chamada da função exibir, herdada da função exibirInfos do objeto user (gera undefined undefined pois está descontextualizada)
-exibir();
-//Declaração de variável e atribuição de função que exibirá o valor da propriedade nome, usando a keyword this. Como não há referencia de onde o this deve contetualizar, haverá erro se não usar o método bind
-const exibirNome = function(){
-    console.log(this.nome);
-}
-//Declarção de variável e atribuição da função exbirNome usando o método bind(arg) que contextualizaráa execução com o objeto do argumento
-const exibirN = exibirNome.bind(user);
-//Evocação da função exibirN()
-exibirN();
+
+const admin = {
+    nome: "Mariana",
+    email: 'm@m.com',
+    nascimento: '2021/05/02',
+    role:'admin',
+    ativo: true,
+    criarCurso(){
+        console.log("Curso criado!");
+    }
+};
+//Uso o método para definir herança via protótipo, onde o objeto admin receberá as propriedades de user
+Object.setPrototypeOf(admin, user);
+//Chamada de método nativo e método herdado
+admin.criarCurso();
+admin.exibirInfos();
