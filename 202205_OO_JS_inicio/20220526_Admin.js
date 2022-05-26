d6658254c1233244586d9f0b58ef3f8430318e6f
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-05-26 19:17:44 -0300

    Atualização de código

--- 202205_OO_JS_inicio/20220526_Admin.js
+++ 202205_OO_JS_inicio/20220526_Admin.js
@@ -6,10 +6,10 @@ import User from './20220526_User.js';
 class Admin extends User{
     //Declaração de método construtor para o intanciamento do objeto User
     constructor(nome, email, nascimento, role = 'administrador', ativo = true){
-        //
+        //Uso da keywordsupper(args) que fará efetvamente a herança dos atributos da superclasse
         super(nome, email, nascimento, role, ativo);
-    }
-}
+    };
+};
 //Instanciamento de novo objeto usando a classe Admin e passando os valores dos atributos
 const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2002-01-03');
 //Exibição dos elemntos de novoAdmin e do retorno da função .exibirInfos() do mesmo objeto
