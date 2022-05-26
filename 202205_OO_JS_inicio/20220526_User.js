5d39ee1323bb10824ec31310064e02cb3bc53b4b
Marcus_Vieira <marcusvmvieira@gmail.com>
2022-05-26 18:51:25 -0300

    Trecho comentado para poder utilizar externamente a classe via exportação

--- 202205_OO_JS_inicio/20220526_User.js
+++ 202205_OO_JS_inicio/20220526_User.js
@@ -1,7 +1,7 @@
 //Exemplo de uso de classes
 
-//Declaração da classe User
-class User{
+//Declaração da classe User com exportação para acesso externo
+export default class User{
     //Declaração da função construtora e seus atributos onde o atributo ativo já receberá o valor ativo ao ser instanciado o objeto, a não ser que algo altere esse estado
     constructor(nome, email, nascimento, role, ativo = true){
         this.nome = nome;
@@ -15,10 +15,11 @@ class User{
         return `${this.nome}, ${this.email}, ${this.ativo}`;
     }
 };
-//Instanciamento de novo objeto novoUser via operador new
-const novoUser = new User('Marcus', 'm@m.com', '1985-08-21');
-//Exibição dos atributos do objeto novoUser e do valor de retorno do método .exibirInfos() deste mesmo objeto
-console.table(novoUser);
-console.log(novoUser.exibirInfos());
-//Exibição se o atributo protótipo de user é o mesmo prótotipo de novoUser
-console.log('User é protótipo de novoUser? ' + User.prototype.isPrototypeOf(novoUser));
+//-----COMENTADO PARA PODER EXPORTAR A CLASSE----
+// //Instanciamento de novo objeto novoUser via operador new
+// const novoUser = new User('Marcus', 'm@m.com', '1985-08-21');
+// //Exibição dos atributos do objeto novoUser e do valor de retorno do método .exibirInfos() deste mesmo objeto
+// console.table(novoUser);
+// console.log(novoUser.exibirInfos());
+// //Exibição se o atributo protótipo de user é o mesmo prótotipo de novoUser
+// console.log('User é protótipo de novoUser? ' + User.prototype.isPrototypeOf(novoUser));
