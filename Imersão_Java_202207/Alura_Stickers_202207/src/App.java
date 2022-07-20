79c0f9bf6e8942499e666d76fead9f62d1e7ea04
Marcus Vieira <marcusvmvieira@gmail.com>
2022-07-20 14:40:19 -0300

    Criação de arquivos com a lógida da aula 01 da imersão java 202207

---
+++ Imersão_Java_202207/Alura_Stickers_202207/src/App.java
@@ -0,0 +1,34 @@
+//Importação das bibliotecas externas
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Fazer conexão HTTP e buscar o responde da API com os top 250 filmes
+        //Declaração de variável com a URL de acesso HTTP à API IMDB. A URL oficial está fora do ar
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        //Criação de uma URI com base em uma url
+        URI endereço = URI.create(url);
+        //Instanciamento de um novo cliente de request via HTTP
+        HttpClient client = HttpClient.newHttpClient();
+        //Instanciamento de um request com argumento sendo o valor da variável URI endereço, realizando o método GET para adquirir os dados
+        HttpRequest request = HttpRequest.newBuilder(endereço).GET().build();
+        //Evocação de método .send interno do objeto client, que enviará o conteúdo da variável request e usasará a classe BodyHandlers para tratar a string, atribuindo o valor de resposta à variável
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        //Atribuição do corpo do conteúdo da resposta à variável body
+        String body = response.body();
+        //Exibição no console do valor da variável body
+        System.out.println(body);
+
+        //Extrair (parsear) os dados relevantes (título, imagem, classificação)
+
+
+        
+
+        //Manipular e exibir os dados
+
+    }
+}
