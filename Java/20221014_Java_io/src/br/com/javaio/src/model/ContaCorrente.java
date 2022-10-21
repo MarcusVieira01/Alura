6271adc79e620ac36b94029fc110852bcf6f2f3a
Servidor <marcusrspprodtech@outlook.com>
2022-10-21 15:29:27 -0300

    Criação de arquivos com a lógica da aula sobre herança da serialização e transient

---
+++ Java/20221014_Java_io/src/br/com/javaio/src/model/ContaCorrente.java
@@ -0,0 +1,58 @@
+//Declaração de pacote
+package br.com.javaio.src.model;
+//Declaração de classe primária
+public class ContaCorrente implements java.io.Serializable{
+    //Declaração de atributos privados
+    //private transient Cliente titular; //COMENTADO PARA REMOVER A DEMONSTRAÇÂO DA NÃO SERIALIZAÇÃO DESSE ATRIBUTO
+    private Cliente titular;
+    private int conta;
+    private int agencia;
+    private double saldo;
+    private static final long serialVersionUID = 2L;
+
+    //Declaração de contrutor não padrão
+    public ContaCorrente(int conta, int agencia){
+        this.conta = conta;
+        this.agencia = agencia;
+    }
+
+    //Declaração de métodos Setter e Getter
+    public Cliente getTitular() {
+        return titular;
+    }
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+    public int getConta() {
+        return conta;
+    }
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
+    public int getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    //Sobreescrita do método toString
+    @Override
+    public String toString() {
+        return "ContaCorrente [titular=" + titular + ", conta=" + conta + ", agencia=" + agencia + ", saldo=" + saldo
+                + "]";
+    }
+
+    //
+    public void deposita(double valor){
+        this.setSaldo(valor + getSaldo());
+        System.out.println("Foi depositado R$" + valor);
+    }
+
+}
---
+++ Java/20221014_Java_io/src/br/com/javaio/src/teste/TesteSerializacaoCCCliente.java
@@ -0,0 +1,44 @@
+//Declaração de pacote
+package br.com.javaio.src.teste;
+
+//Importação de classes externas
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+//Importação de classe interna ao projeto
+import br.com.javaio.src.model.Cliente;
+import br.com.javaio.src.model.ContaCorrente;
+
+//Declaração de classe primária
+public class TesteSerializacaoCCCliente {
+    //Declaração de método principal
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+        //Instanciação de objeto via construtor Cliente(args) da classe Cleinte com atribuição de sua referência à variável declarada do tipo Cliente
+        Cliente cliente = new Cliente("Marcus Vieira", "123456789-41");
+
+        //Instanciação de objeto via construtor ContaCorrente(args) da classe ContaCorrente com atribuição de sua referência à variável declarada do tipo ContaCorrente
+        ContaCorrente cc = new ContaCorrente(123, 9999);
+        //Evocação de métodos do objeto para população de atributos
+        cc.setTitular(cliente);
+        cc.deposita(250.56);
+
+        //Instanciação de objeto, de referência atribuída à variável oos, via construtor onde foi passado um novo objeto instanciado da classe FileOutputStream para definir como será a saída
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("contacorrente.bin"));
+        //Método evocado para transformar o objeto de referência contida na variável cc em um fluxo binário, pelo objeto de referêncaia oos contida em oos
+        oos.writeObject(cc);
+        //Evocação de método para fechar o acesso ao objeto de referência contida em oos
+        oos.close();
+
+        //Instanciação de objeto, de referência atribuída à variável ois, via construtor onde foi passado um novo objeto instanciado da classe FileInputStream para definir como será a entrada
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("contacorrente.bin"));
+        //Declaração de variável com atribuição do valor de retorno do método readObject() que retornará um objeto genérido. Sendo assim usamos o Cast para ContaCorrente para que seu valor de retorno seja do mesmo tipo da declaração de variável; 
+        ContaCorrente contaLido = (ContaCorrente) ois.readObject();
+        //Evocação de método para fechar o acesso ao objeto de referência contida em ois
+        ois.close();
+        //Exibição do valor atribuído à variável contaLido
+        System.out.println(contaLido);
+    }
+}
