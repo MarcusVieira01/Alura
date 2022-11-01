e9ab1942fe5814b202defa4f6f1d4390012df302
Servidor <marcusrspprodtech@outlook.com>
2022-11-01 20:05:26 -0300

    Atualização de arquivo implementando o método .coparring(function) que recebe um lambda

--- Java/20221101_Java8/src/OrdenaStrings.java
+++ Java/20221101_Java8/src/OrdenaStrings.java
@@ -1,5 +1,6 @@
 //Inportação de classes externas
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 //Declaração de classe principal
@@ -19,15 +20,19 @@ public class OrdenaStrings {
         //Evocação de método .sort(obj), que consta na classe auxiliar Collections, que fará a ordenação lexográfica (alfabética)
         //Collections.sort(palavras, comparador);
 
+        //COMENTADO PARA PODER IMPLEMENTAR A LINHA ABAIXO
         //Evocação do método .sort(arg) via a variável que possui a referência do objeto palavras, sem acesso á classe auxiliar Collections e implementando a interface Comparator via lâmbda. Evocação do método .compare(args) da classe wrapper Integer, que fará a comparação dos retornos dos métodos .length() e retornará -1 caso s2 seja maior, 1 caso s1 seja maior e 0 caso sejam iguais
-        palavras.sort((s1, s2) -> {
-            return Integer.compare(s1.length(), s2.length());
-        });
+        // palavras.sort((s1, s2) -> {
+        //     return Integer.compare(s1.length(), s2.length());
+        // });
+
+        //Código similar com o acima, porém usando factoring. Evocação do método .sort(arg) via a variável que possui a referência do objeto palavras. É passado como parâmetro o retorno do método .comparing da classe Comparator. Esse método recebe um lambda como parâmetro
+        palavras.sort(Comparator.comparing(s -> s.length()));
         
         //Exibição do conteúdo do objeto de referência atribuída à variável passada como argumento
         System.out.println(palavras);
 
-        //Evocação de método .forEach(consumer) do objeto de referência armazenada na variável palavras onde é passado um LAMBDA de uma interface que, como o método .forEach orbigatóriamente precisa receber um Consumer, sua implementão é implícita. LAMBDA COMPLETO
+        //Evocação de método .forEach(consumer) do objeto de referência armazenada na variável palavras onde é passado um LAMBDA de uma interface que, como o método .forEach obrigatóriamente precisa receber um Consumer, sua implementão é implícita. LAMBDA COMPLETO
         palavras.forEach((String s) -> {
             System.out.println(s);
         });
