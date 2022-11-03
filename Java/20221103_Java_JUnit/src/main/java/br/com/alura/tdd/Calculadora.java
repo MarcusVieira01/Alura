3452f0a83be2e4c10a87dd9f654694ebe73d8937
Servidor <marcusrspprodtech@outlook.com>
2022-11-03 10:00:50 -0300

    Criação de arquivos com a lógica da aula sobre Escrevendo Testes Automatizados Simples

---
+++ Java/20221103_Java_JUnit/src/main/java/br/com/alura/tdd/Calculadora.java
@@ -0,0 +1,10 @@
+//Declaração de pacote
+package br.com.alura.tdd;
+
+public class Calculadora {
+    //Declaração de método manual
+    public int somar(int a, int b){
+        return a + b;
+    }
+    
+}
