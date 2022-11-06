fff465809df536a1ecdd43893c56d062e202ef10
Servidor <marcusrspprodtech@outlook.com>
2022-11-06 19:46:53 -0300

    Modificação dos arquivos com a lógica da aulas sobre organização de código e refatoração

--- Java/20221103_Java_JUnit/src/test/java/br/com/alura/tdd/java/BonusServiceTest.java
+++ Java/20221103_Java_JUnit/src/test/java/br/com/alura/tdd/java/BonusServiceTest.java
@@ -1,61 +1,49 @@
 //Declaração de pacote
 package br.com.alura.tdd.java;
 
-//Importação de classes
+//Importação de classes externas
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-//Importação de classes para teste
-import org.junit.jupiter.api.Test;
-
+//Importação de classes internas
 import br.com.alura.tdd.modelo.Funcionario;
 import br.com.alura.tdd.service.BonusService;
+
+//Importação de classes para teste
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 //import static org.junit.jupiter.api.Assertions.fail; //COMENTADO PARA NÂO ENTRAR EM WARNING
 
 //Declaração de classe de teste
 public class BonusServiceTest {
-    //Declaração de método de teste com a anotação @Test
-    //TEST DECLARADO COMENTADO DEVIDO À NOVA IMPLEMENTAÇÃO DO CÁLCULO DO BONUS
-    // @Test
-    // void bonusDeveriaSerZeroSalarioAlto(){
-    //     //Instanciação de objeto via construtor e atribuição do valor de refer~encia à variável declarada
-    //     BonusService service = new BonusService();
-    //     //Declaração de variável com atribuição do valor de retorno do método .calcularBonus(args)
-    //     BigDecimal bonus = service.calcularBonus(new Funcionario("Marcus Vieira", LocalDate.now(), new BigDecimal("25000")));
-    //     //Evocação do método assert.Equals(args) para conferência do valor da variável bonus
-    //     assertEquals(new BigDecimal("0.00"), bonus);
-    // }
+    //Declaração de atrubutos, para regatoração
+    private BonusService service;
+    private Funcionario funcionario;
+
+    //Declaração de método onde serão instanciados os objetos pertinentes e atribuídas suas referências nos valores dos atributos pertinentes
+    public void inicializar(String salario){
+        this.service = new BonusService();
+        this.funcionario = new Funcionario("Marcus Vieira", LocalDate.now(), new BigDecimal(salario));
+    }
+
     //Declaração de método de teste com a anotação @Test
     @Test
     void bonusDeveriaSerZeroSalarioAlto(){
-        //Instanciação de objeto via construtor e atribuição do valor de refer~encia à variável declarada
-        BonusService service = new BonusService();
+        //Evocação de método para instaniação dos objetos necessários
+        inicializar("25000");
         //Evocação do método .calcularBonus(args) dentro do argumento do método assertThrows(args) que testará se o retorno do método evocado é a excessão desejada
         assertThrows(IllegalArgumentException.class, 
-        () -> service.calcularBonus(new Funcionario("Marcus Vieira", LocalDate.now(), new BigDecimal("25000"))));
-
-        // //COMENTADO PARA NÃO EXECUTAR COMO ERRO
-        // //Estrutura try-catch que tratará o lançamento de exceção conforme a lógica do método evocado no bloco try 
-        // try{
-        //     //Método .calcularBonus(args) evocado
-        //     service.calcularBonus(new Funcionario("Marcus Vieira", LocalDate.now(), new BigDecimal("25000")));
-        //     //Método evocado para ocasionar falha deliberada no teste
-        //     fail("Não executou a exception");
-        // }catch(Exception e){
-        //     assertEquals("Funcionário impossibilitado de receber bonus", e.getMessage());
-        // }
-
+        () -> service.calcularBonus(funcionario));
     }
 
     //Declaração de método de teste com a anotação @Test
     @Test
     void bonusDeveriaSerDezPorcentoSalario(){
-        //Instanciação de objeto via construtor e atribuição do valor de refer~encia à variável declarada
-        BonusService service = new BonusService();
+        //Evocação de método para instaniação dos objetos necessários com o valor do cenário
+        inicializar("2500");
         //Declaração de variável com atribuição do valor de retorno do método .calcularBonus(args)
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Marcus Vieira", LocalDate.now(), new BigDecimal("2500")));
+        BigDecimal bonus = service.calcularBonus(funcionario);
         //Evocação do método assert.Equals(args) para conferência do valor da variável bonus
         assertEquals(new BigDecimal("250.00"), bonus);
     }
@@ -63,10 +51,10 @@ public class BonusServiceTest {
     //Declaração de método de teste com a anotação @Test
     @Test
     void bonusMaximo(){
-        //Instanciação de objeto via construtor e atribuição do valor de refer~encia à variável declarada
-        BonusService service = new BonusService();
+        //Evocação de método para instaniação dos objetos necessários com o valor do cenário
+        inicializar("10000");
         //Declaração de variável com atribuição do valor de retorno do método .calcularBonus(args)
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Marcus Vieira", LocalDate.now(), new BigDecimal("10000")));
+        BigDecimal bonus = service.calcularBonus(funcionario);
         //Evocação do método assert.Equals(args) para conferência do valor da variável bonus
         assertEquals(new BigDecimal("1000.00"), bonus);
     }
--- Java/20221103_Java_JUnit/src/test/java/br/com/alura/tdd/java/ReajusteServiceTest.java
+++ Java/20221103_Java_JUnit/src/test/java/br/com/alura/tdd/java/ReajusteServiceTest.java
@@ -13,15 +13,25 @@ import br.com.alura.tdd.modelo.Desempenho;
 //Importação de classes para teste
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
 
 //Declaração de classe de teste
 public class ReajusteServiceTest {
+    //Declaração de atrb=ibutos privados
+    private ReajusteService service;
+    private Funcionario funcionario;
+
+    //Uso da anotação @BeforeEach para que este método seja instanciando antes de todos os métodos de teste
+    @BeforeEach
+    //Declaração de método onde serão instanciados os objetos pertinentes e atribuídas suas referências nos valores dos atributos pertinentes
+    public void inicialiar(){
+        this.service = new ReajusteService();
+        this.funcionario = new Funcionario("Marcus", LocalDate.now(), new BigDecimal("1000.00"));
+    }
+
     //Declaração de método de teste para realiste com desempenho a desejar
     @Test
     public void reajusteTresPorcentoDesejar(){
-        //Declaraçãod e ariável e atribuiçãod o valor de referência do objeto instanciado via construtor
-        ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Marcus", LocalDate.now(), new BigDecimal("1000.00"));
         //Evocação do método .concedeReajuste(args) da classe ReajusteService, que fará o reajuste conforme o desempenho dado pelo valor do emum Desempenho.A_DESEJAR
         service.concedeReajuste(funcionario, Desempenho.A_DESEJAR);
         //Evocação de método assertEquals(args) que validará se o retorno do método da classe testada, no caso ReajusteService, é o esperado
@@ -31,9 +41,6 @@ public class ReajusteServiceTest {
     //Declaração de método de teste para realiste com desempenho bom
     @Test
     public void reajusteQuinzePorcentoBom(){
-        //Declaraçãod e ariável e atribuiçãod o valor de referência do objeto instanciado via construtor
-        ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Marcus", LocalDate.now(), new BigDecimal("1000.00"));
         //Evocação do método .concedeReajuste(args) da classe ReajusteService, que fará o reajuste conforme o desempenho dado pelo valor do emum Desempenho.BOM
         service.concedeReajuste(funcionario, Desempenho.BOM);
         //Evocação de método assertEquals(args) que validará se o retorno do método da classe testada, no caso ReajusteService, é o esperado
@@ -43,9 +50,6 @@ public class ReajusteServiceTest {
     //Declaração de método de teste para realiste com desempenho excelente
     @Test
     public void reajusteVintePorcentoExelente(){
-        //Declaraçãod e ariável e atribuiçãod o valor de referência do objeto instanciado via construtor
-        ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Marcus", LocalDate.now(), new BigDecimal("1000.00"));
         //Evocação do método .concedeReajuste(args) da classe ReajusteService, que fará o reajuste conforme o desempenho dado pelo valor do emum Desempenho.OTIMO
         service.concedeReajuste(funcionario, Desempenho.OTIMO);
         //Evocação de método assertEquals(args) que validará se o retorno do método da classe testada, no caso ReajusteService, é o esperado
@@ -55,9 +59,6 @@ public class ReajusteServiceTest {
     //Declaração de método de teste para realiste com desempenho excelente
     @Test
     public void reajusteVintePorcentoEspetacular(){
-        //Declaraçãod e ariável e atribuiçãod o valor de referência do objeto instanciado via construtor
-        ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Marcus", LocalDate.now(), new BigDecimal("1000.00"));
         //Evocação do método .concedeReajuste(args) da classe ReajusteService, que fará o reajuste conforme o desempenho dado pelo valor do emum Desempenho.OTIMO
         service.concedeReajuste(funcionario, Desempenho.ESPETACULAR);
         //Evocação de método assertEquals(args) que validará se o retorno do método da classe testada, no caso ReajusteService, é o esperado
