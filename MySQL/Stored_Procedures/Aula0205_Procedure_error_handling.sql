0535205dfa438c18d5b8116aa5b136e27c8b4353
Marcus Vieira <marcusvmvieira@gmail.com>
2022-12-27 13:20:02 -0300

    Criação de arquivos com a lógica da aula sobre Stored Procedures

---
+++ MySQL/Stored_Procedures/Aula0205_Procedure_error_handling.sql
@@ -0,0 +1,4 @@
+call inclui_novo_produto_parametro('4000003','Sabor do Pantanal 1 Litro - Melancia','Melancia','1 litro','Pet',4.67);
+
+SELECT * FROM tabela_de_produtos 
+WHERE NOME_DO_PRODUTO LIKE 'Sabor do Pantanal%';
