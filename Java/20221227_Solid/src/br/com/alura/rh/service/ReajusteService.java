580948955cce265dbc502419b182d91c114fe0e3
Marcus Vieira <marcusvmvieira@gmail.com>
2023-01-11 16:48:22 -0300

    Regras de validação passadas para classes externas, aumentando a coesão e o princípio de única responsabilidade

--- Java/20221227_Solid/src/br/com/alura/rh/service/ReajusteService.java
+++ Java/20221227_Solid/src/br/com/alura/rh/service/ReajusteService.java
@@ -4,6 +4,10 @@ package br.com.alura.rh.service;
 //Importação de classes externas
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
 //Importação de classes internas
 import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
@@ -12,25 +16,24 @@ import br.com.alura.rh.model.Funcionario;
  * @apiNote Classe que realiza a reponsabilidade de validar um reajuste perante ao atributo salario de um funcionário 
  */
 public class ReajusteService {
+
+    private List<ValidacaoReajuste> validacoes;
+
+    public ReajusteService(List<ValidacaoReajuste> validacoes){
+        this.validacoes = validacoes;
+    }
+
     /**
      * Método que realiza a validação de um reajuste salarial usando a comparação do valor do parametro reajuste com o valor do atributo saladia do objeto funcionário, também passado como parâmetro
      * @param funcionario
      * @param aumento
      */
 	public void reajustarSalario(Funcionario funcionario, BigDecimal aumento) {
-        //Declaração de variável e atribuição do valor de retorno do mpétodo .getSalario() do objeto funcionário da classe Funcionário
-		BigDecimal salarioAtual = funcionario.getSalario();
-
-        //Declaração de variável e atribuição do valor de retorno do método divide(args) do objeto aumento, passado como parâmetro
-		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-
-		//Condicional que verifica se o reajuste está fora do permitido e caso true, lança exceção. Aplicado FAIL FAST
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
+        
+        this.validacoes.forEach(v -> v.validar(funcionario, aumento));
 
         //Declaração de variável e atribuição do valor de retorno do método .add(args)
-        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
 
 		//Evocação de método da classe Funcionário, via objeto funcionario para realizar a alteração do valor do atributo salário
         funcionario.atualizarSalario(salarioReajustado);
