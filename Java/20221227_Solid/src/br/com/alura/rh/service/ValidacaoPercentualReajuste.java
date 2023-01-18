5f80934bace85cc2acd51ab49dd1555738a1dce3
Marcus Vieira <marcusvmvieira@gmail.com>
2023-01-18 14:13:07 -0300

    Atualização de comentários

--- Java/20221227_Solid/src/br/com/alura/rh/service/ValidacaoPercentualReajuste.java
+++ Java/20221227_Solid/src/br/com/alura/rh/service/ValidacaoPercentualReajuste.java
@@ -1,13 +1,20 @@
+//Declaração de pacote
 package br.com.alura.rh.service;
 
+//Importação de classes externas
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-
+//Importação de classes internas
 import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
+/**
+ * @apiNote Declaração de classe que implementa a interface ValidacaoReajuste e define o método de validação para o % de reajuste
+ */
 public class ValidacaoPercentualReajuste implements ValidacaoReajuste{
-
+    /**
+     * Método que realiza a validação da % de reajust
+     */
     public void validar(Funcionario funcionario, BigDecimal aumento) {
         //Declaração de variável e atribuição do valor de retorno do mpétodo .getSalario() do objeto funcionário da classe Funcionário
         BigDecimal salarioAtual = funcionario.getSalario();
@@ -15,11 +22,9 @@ public class ValidacaoPercentualReajuste implements ValidacaoReajuste{
         //Declaração de variável e atribuição do valor de retorno do método divide(args) do objeto aumento, passado como parâmetro
         BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
     
-        //Condicional que verifica se o reajuste está fora do permitido e caso true, lança exceção. Aplicado FAIL FAST
+        //implementação de fail fast via condicional que verifica se o reajuste está fora do permitido e caso true, lança exceção
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
             throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
         }
-
     }
-    
 }
