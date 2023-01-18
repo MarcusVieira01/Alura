b5f1c61ff9943d30c2b8fc5cabf7bdade251ff70
Marcus Vieira <marcusvmvieira@gmail.com>
2023-01-18 14:06:08 -0300

    Atualização de comentários

--- Java/20221227_Solid/src/br/com/alura/rh/service/ValidacaoReajuste.java
+++ Java/20221227_Solid/src/br/com/alura/rh/service/ValidacaoReajuste.java
@@ -1,10 +1,19 @@
+//Declaração de pacote
 package br.com.alura.rh.service;
 
+//Importação de classe externa 
 import java.math.BigDecimal;
-
+//Importação de classe interna
 import br.com.alura.rh.model.Funcionario;
 
+/**
+ * @apiNote Interface de padronização para as validações
+ */
 public interface ValidacaoReajuste {
-
+    /**
+     * Método de validação abstrato a ser implementado em cada classe de validação
+     * @param funcionario
+     * @param aumento
+     */
     void validar(Funcionario funcionario, BigDecimal aumento);
 }
