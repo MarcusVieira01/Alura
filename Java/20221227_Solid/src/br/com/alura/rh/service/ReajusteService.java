d484f6b980fd1184cf8317936ddb1b3d96cad3c8
Marcus Vieira <marcusvmvieira@gmail.com>
2023-01-18 14:04:04 -0300

    Refatoração do código para que seja aceito uma lista de validações provenientes das classes de validação

--- Java/20221227_Solid/src/br/com/alura/rh/service/ReajusteService.java
+++ Java/20221227_Solid/src/br/com/alura/rh/service/ReajusteService.java
@@ -12,20 +12,24 @@ import br.com.alura.rh.model.Funcionario;
  * @apiNote Classe que realiza a reponsabilidade de validar um reajuste perante ao atributo salario de um funcionário 
  */
 public class ReajusteService {
-
+    //Declaração d atributos
     private List<ValidacaoReajuste> validacoes;
 
+    /**
+     * Declaração de construtor
+     * @param validacoes
+     */
     public ReajusteService(List<ValidacaoReajuste> validacoes){
         this.validacoes = validacoes;
     }
 
     /**
-     * Método que realiza a validação de um reajuste salarial usando a comparação do valor do parametro reajuste com o valor do atributo saladia do objeto funcionário, também passado como parâmetro
+     * Método que realiza a validação de um reajuste salarial usando a evocação via lâmbda de cada validação contida no atributo validacoes e modifica o valor do atributo salário do objeto Funcionário via método atualizarSalario()
      * @param funcionario
      * @param aumento
      */
 	public void reajustarSalario(Funcionario funcionario, BigDecimal aumento) {
-        
+        //Atribuição via lâmbda do retorno da fução .validar(args) em cada iteração do loop foreach
         this.validacoes.forEach(v -> v.validar(funcionario, aumento));
 
         //Declaração de variável e atribuição do valor de retorno do método .add(args)
