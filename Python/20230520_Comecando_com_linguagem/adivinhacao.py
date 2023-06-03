4a78e74c848dd9cc09f88101a7485e91903a9284
Marcus Vieira <marcusvmvieira@gmail.com>
2023-06-02 22:27:53 -0300

    Alteração do código para uso da keyword break

--- Python/20230520_Comecando_com_linguagem/adivinhacao.py
+++ Python/20230520_Comecando_com_linguagem/adivinhacao.py
@@ -1,23 +1,27 @@
 # Imprime mensagem de boas vindas
 print("Bem vindo ao jogo de adivinhação!")
+
 # Definição do número secreto à ser adivinhado
 numeroSecreto = 42
 tentativas = 3
+
 # Evoca o método input e atribui seu valor de retorno à variável chute
-chute = input("Digite o seu número: ")
+chute = int(input("Digite o seu número: "))
+
 # Loop while que fara iterações enquanto a condição for true
 for palpites in range(1, tentativas):
     palpites+=1
     # Condicional que exibirá a mensagem pertinente
-    if int(chute) == numeroSecreto:
+    if chute == numeroSecreto:
         print("Acertô mizerarvi!")
-        exit()
-    elif int(chute) > numeroSecreto:
+        break
+    elif chute > numeroSecreto:
         print(" Seu chute é maior que o número secreto!")
         chute = input("Digite o seu {}º número: ".format(palpites))
-    elif int(chute) < numeroSecreto:
+    elif chute < numeroSecreto:
         print(" Seu chute é menor que o número secreto!")
         chute = input("Digite o seu {}º número: ".format(palpites))
+        
 # Condicional que exibirá a pensagem de jogo eprdido
-if tentativas >= 3:
+if palpites >= 3:
     print("Erroooooou tudo!")
