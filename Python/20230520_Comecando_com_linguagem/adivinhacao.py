73b9f70045eeec92fa3934245de1b56b14e4921f
Marcus Vieira <marcusvmvieira@gmail.com>
2023-06-02 22:47:22 -0300

    Alteração do código para geração pseudorandômica para a variável numero_secreto

--- Python/20230520_Comecando_com_linguagem/adivinhacao.py
+++ Python/20230520_Comecando_com_linguagem/adivinhacao.py
@@ -1,8 +1,11 @@
+# Importação de pacotes externos
+import random
+
 # Imprime mensagem de boas vindas
 print("Bem vindo ao jogo de adivinhação!")
 
 # Definição do número secreto à ser adivinhado
-numeroSecreto = 42
+numero_secreto = random.randint(1,50)
 tentativas = 3
 
 # Evoca o método input e atribui seu valor de retorno à variável chute
@@ -12,16 +15,16 @@ chute = int(input("Digite o seu número: "))
 for palpites in range(1, tentativas):
     palpites+=1
     # Condicional que exibirá a mensagem pertinente
-    if chute == numeroSecreto:
+    if chute == numero_secreto:
         print("Acertô mizerarvi!")
         break
-    elif chute > numeroSecreto:
+    elif chute > numero_secreto:
         print(" Seu chute é maior que o número secreto!")
         chute = int(input(f"Digite o seu {palpites}º número: "))
-    elif chute < numeroSecreto:
+    elif chute < numero_secreto:
         print(" Seu chute é menor que o número secreto!")
         chute = int(input(f"Digite o seu {palpites}º número: "))
 
 # Condicional que exibirá a pensagem de jogo eprdido
 if palpites >= 3:
-    print("Erroooooou tudo!")
+    print(f"Erroooooou tudo! O número secreto é {numero_secreto}")
