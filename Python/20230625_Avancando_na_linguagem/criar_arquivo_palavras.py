6a31094af1e4b221de8ef9dc7b4ba9e7a9eb8845
Marcus Vieira <marcusvmvieira@gmail.com>
2023-07-08 16:24:02 -0300

    Implementação de lógica para criação de arquivo txt e o arquivo resultado da implementação

---
+++ Python/20230625_Avancando_na_linguagem/criar_arquivo_palavras.py
@@ -0,0 +1,10 @@
+#
+arquivo = open("./palavras.txt", "w", encoding="utf-8")
+
+#
+arquivo.write("banana\n")
+arquivo.write("melancia\n")
+arquivo.write("morango\n")
+arquivo.write("manga\n")
+
+arquivo.close()
