0d7a585e08ff66cb87d8bda95b4c735eba936f13
Marcus Vieira <marcusvmvieira@gmail.com>
2023-07-08 16:18:11 -0300

    Implementação da criação automática para o conteúdo da lista letras_acertadas e implementação de final de jogo

--- Python/20230625_Avancando_na_linguagem/forca.py
+++ Python/20230625_Avancando_na_linguagem/forca.py
@@ -6,37 +6,41 @@ def jogar():
     print("*********************************")
 
     # Declaração de variáveis e suas inicializações
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     enforcou = False
     acertou = False
-    letras_acertadas = ["_","_","_","_","_","_"]
-
+    erros = 0
     #
-    print(letras_acertadas,)
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    # Exibe a sequencia contida na lista letras_acertadas
+    print(letras_acertadas)
 
     # Loop que executará a lógica do jogo
     while(not enforcou and not acertou):
         # Captura o input do usuário e atribui à variável executando limpeza de dados
         chute = input("Qual a letra?")
-        chute = chute.lower()
-        chute = chute.strip()
-        # Declaração de variável local ao loop while
-        index = 0
-        # Loop que varrerá a variável palavra_secreta e executará o condicional que exibirá qual a
-        # letra chutada e o índice na palavra secreta, caso seja true
-        for letra in palavra_secreta:
-            # Condicional que verificará se o chute tem mesmo valor da letra no lacço for
-            if chute == letra:
-                letras_acertadas[index] = letra
-            # Incremento unitário na variável index
-            index += 1
-        # 
+        chute = chute.strip().upper()
+        # Condicional define se a letra chutada está dentro da cadeia contida em palavra_secreta. Caso false, soma 1 no acumulador de erros
+        if chute in palavra_secreta:
+            # Declaração de variável local ao loop while
+            index = 0
+            # Loop que varrerá a variável palavra_secreta e executará o condicional que exibirá qual a letra chutada e o índice na palavra secreta, caso seja true
+            for letra in palavra_secreta:
+                # Condicional que verificará se o chute tem mesmo valor da letra no lacço for
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                # Incremento unitário na variável index
+                index += 1
+        else:
+            erros += 1
+        # Atribuição do valor de retorno da comparação lógica para definir se o jogador perdeu
+        enforcou = erros == len(palavra_secreta)
+        # Atribuição do valor de retorno da comparação lógica para definir se o jogador ganhou
+        acertou = "_" not in letras_acertadas
+        # Exibição da última atualização do conteúdo da variáveç letras_acertadas
         print(letras_acertadas)
-
-
-
-
-
+    # Exibição de mensagem
     print("Fim do jogo")
 
 # Condicional que verificará se a variável interna do Python possui um valor específico, onde caso true executará a função.
