e1d27c723e5bc14e6614865f1b15d0e759d1841f
Marcus Vieira <marcusvmvieira@gmail.com>
2023-07-08 19:33:59 -0300

    Implementação com particionamento do código em funções

--- Python/20230625_Avancando_na_linguagem/forca.py
+++ Python/20230625_Avancando_na_linguagem/forca.py
@@ -1,13 +1,16 @@
 '''Descrição do módulo: Jogo de forca'''
-#
+# Importação de módulo externo
 import random
 
-def jogar():
-    '''Descrição da função jogar: Definição de função com a lógica do jogo de forca'''
+def abertura():
+    '''Descrição da função: Exibir mensagem de entrada'''
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+
+def carrega_palavra_secreta():
+    '''Descrição da função: carrega palavra secreta'''
     # Abertura do arquivo txt como apenas leitura e atribuição à variável arquivo com uso de with, para fechamento automático do arquivo
     with open("./palavras.txt", "r", encoding="utf-8") as arquivo:
 
@@ -22,13 +25,25 @@ def jogar():
     # Atribuição à variável indice o valor pseudorandômico de retorno da função randrange(args)
     indice = random.randrange(0,len(palavras),1)
 
-    # Atribuição do valor do elemento dado pelo conteúdo da variável indice
-    palavra_secreta = palavras[indice].upper()
+    # Retorno da palavra secreta
+    return palavras[indice].upper()
+
+
+def jogar():
+    '''Descrição da função jogar: Definição de função com a lógica do jogo de forca'''
+    # Evoca o método de abertura
+    abertura()
+
+    # Evoca o método para definir a palavra secreta
+    carrega_palavra_secreta()
 
     # Declaração de variáveis e suas inicializações
     enforcou = False
     acertou = False
     erros = 0
+
+    # Atribuição do valor de retorno da função
+    palavra_secreta = carrega_palavra_secreta()
 
     # Loop interno à lista para geração de _ para cada caractere do conteúdo da variável palavra_secreta
     letras_acertadas = ["_" for letra in palavra_secreta]
