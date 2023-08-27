2731c04bf7d7a331ed5014696b1d5da50cb15c37
Marcus Vieira <marcusvmvieira@gmail.com>
2023-08-27 11:18:25 -0300

    Implementação de método extrato()

--- Python/20230826_Entendendo_POO/Conta.py
+++ Python/20230826_Entendendo_POO/Conta.py
@@ -1,7 +1,12 @@
 class Conta:
+    # Declaração de classe construtora
     def __init__(self, numero, titular, saldo, limite):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("O saldo da conta {} é R${}.".format(self.titular, self.saldo))
+
         
