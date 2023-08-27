859bee3d09fc2e594f6958c61a68f829c7575616
Marcus Vieira <marcusvmvieira@gmail.com>
2023-08-27 12:00:26 -0300

    Inclusão de comentários, após alteração do PyLint com as convenções passando de Information para Hint

--- Python/20230826_Entendendo_POO/Conta.py
+++ Python/20230826_Entendendo_POO/Conta.py
@@ -1,17 +1,18 @@
+#Classe que abstrai o conceito de conta bancária com os atributos numero de iedentificação, titular, saldo atual e limite de crédito"
 class Conta:
-    # Declaração de classe construtora
+    # Declaração de construtor com os atributos da classe
     def __init__(self, numero, titular, saldo, limite):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
-
+    # Declaração de método que exibe o titular e o saldo da conta
     def extrato(self):
         print("O saldo da conta {} é R${}.".format(self.titular, self.saldo))
-
+    # Declaração de método que fará o incremento de um valor ao atributo saldo
     def deposita(self, valorDeposita):
         self.saldo += valorDeposita
-
+    # Declaração de método que fará o decréscimo de um valor ao atributo saldo
     def saca(self, valorSaca):
         self.saldo -= valorSaca
 
