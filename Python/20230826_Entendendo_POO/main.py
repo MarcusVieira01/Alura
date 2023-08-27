5c20de847fea07a3dd1434d7dc682cf3eb105a8a
Marcus Vieira <marcusvmvieira@gmail.com>
2023-08-27 11:37:02 -0300

    Evocação dos métodos saca() e deposita() com posterior exibição do saldo

--- Python/20230826_Entendendo_POO/main.py
+++ Python/20230826_Entendendo_POO/main.py
@@ -4,3 +4,10 @@ contaMarcus = Conta(123, "Marcus", 120.00, 250.00)
 
 contaMarcus.extrato()
 
+contaMarcus.saca(50.00)
+
+contaMarcus.extrato()
+
+contaMarcus.deposita(100.00)
+
+contaMarcus.extrato()
