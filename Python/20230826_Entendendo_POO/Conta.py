a6b6b9131be402afba2a013e68e01aa26ac84dca
Marcus Vieira <marcusvmvieira@gmail.com>
2023-08-27 12:26:26 -0300

    Alteração da visibilidade dos atributos de public para private

--- Python/20230826_Entendendo_POO/Conta.py
+++ Python/20230826_Entendendo_POO/Conta.py
@@ -1,19 +1,19 @@
-#Classe que abstrai o conceito de conta bancária com os atributos numero de iedentificação, titular, saldo atual e limite de crédito"
+# Classe que abstrai o conceito de conta bancária com os atributos __numero de iedentificação, titular, saldo atual e limite de crédito"
 class Conta:
     # Declaração de construtor com os atributos da classe
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
     # Declaração de método que exibe o titular e o saldo da conta
     def extrato(self):
-        print("O saldo da conta {} é R${}.".format(self.titular, self.saldo))
+        print("O saldo da conta {} é R${}.".format(self.__titular, self.__saldo))
     # Declaração de método que fará o incremento de um valor ao atributo saldo
     def deposita(self, valorDeposita):
-        self.saldo += valorDeposita
+        self.__saldo += valorDeposita
     # Declaração de método que fará o decréscimo de um valor ao atributo saldo
     def saca(self, valorSaca):
-        self.saldo -= valorSaca
+        self.__saldo -= valorSaca
 
     
