72f2f7b9147d9c399c4d8e04004dc0d8e8872842
Marcus Vieira <marcusvmvieira@gmail.com>
2023-08-29 20:57:56 -0300

    Criação de classes que abstraem os conceitos de Filme e Série

---
+++ Python/20230829_Avancando_POO/Filme.py
@@ -0,0 +1,9 @@
+class Filme:
+    # Declaração do construtor
+    def __init__(self, nome, ano, duracao):
+        self.__nome = nome
+        self.__ano = ano
+        self.__duracao = duracao
+
+    
+
---
+++ Python/20230829_Avancando_POO/Serie.py
@@ -0,0 +1,6 @@
+class Serie:
+    # Declaração do construtor
+    def __init__(self, nome, ano, temporadas):
+        self.__nome = nome
+        self.__ano = ano
+        self.__temporadas = temporadas
