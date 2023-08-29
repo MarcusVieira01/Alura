135551a93b1c285580f66aad5ab57fd9a2a2c891
Marcus Vieira <marcusvmvieira@gmail.com>
2023-08-29 20:35:58 -0300

    Implementação de código de exemplo

--- Python/20230826_Entendendo_POO/main.py
+++ Python/20230826_Entendendo_POO/main.py
@@ -1,10 +1,9 @@
 from Conta import Conta
 
+print("Os códigos dos bancos são {}".format(Conta.codigoBanco()))
+
 contaMarcus = Conta(123, "Marcus", 120.00, 250.00)
 
-contaNico = Conta(321, "Nico", 200.00, 1000.00)
-
-contaMarcus.transfere(contaNico, 20.00)
-
+contaMarcus.saca(371.00)
 contaMarcus.extrato()
-contaNico.extrato()
+
