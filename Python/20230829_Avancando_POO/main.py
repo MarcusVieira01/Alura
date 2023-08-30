badd3074e6a375b1d25a8644b63e43827fe499f0
Marcus Vieira <marcusvmvieira@gmail.com>
2023-08-29 21:35:29 -0300

    Alteração de código de exemplo

--- Python/20230829_Avancando_POO/main.py
+++ Python/20230829_Avancando_POO/main.py
@@ -4,8 +4,11 @@ from Serie import Serie
 vingadores = Filme("Vingadores", 2018, 160)
 atlanta = Serie("Atlanta", 2018, 2)
 
-print(vingadores)
-print(atlanta)
+print(vingadores.nome)
+print(atlanta.nome)
 
 vingadores.darLike()
 atlanta.darLike()
+
+print(f"{vingadores.nome} - likes {vingadores.likes}")
+print(f"{atlanta.nome} - likes {atlanta.likes}")
