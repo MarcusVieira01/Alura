d50d453c6ea55c994d382342f6793143aa17d0f5
Marcus Vieira <marcusvmvieira@gmail.com>
2023-08-29 21:16:00 -0300

    Implementação de método darLike() e métodos setter e getter

--- Python/20230829_Avancando_POO/Filme.py
+++ Python/20230829_Avancando_POO/Filme.py
@@ -4,6 +4,32 @@ class Filme:
         self.__nome = nome
         self.__ano = ano
         self.__duracao = duracao
+        self.likes = 0
 
+    # Métodos públicos
+    # Método que adiciona um like
+    def darLike(self):
+        self.likes += 1
+
+    # Métodos getter e setter
+    # Métodos getter
+    @property
+    def nome(self):
+        return self.__nome
+    @property
+    def ano(self):
+        return self.__ano
+    @property
+    def duracao(self):
+        return self.__duracao
     
-
+    # Métodos setter
+    @nome.setter
+    def nome(self,valor):
+        self.__nome = valor
+    @ano.setter
+    def ano(self, valor):
+        self.__ano = valor
+    @duracao.setter
+    def duracao(self, valor):
+        self.__duracao = valor
