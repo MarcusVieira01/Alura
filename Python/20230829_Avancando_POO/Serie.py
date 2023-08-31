e5b13b5a1e23bd6bfd401aa92820044ce3a068cb
Marcus Vieira <marcusvmvieira@gmail.com>
2023-08-30 22:03:54 -0300

    Implementação de herança e redução de duplicação de código

--- Python/20230829_Avancando_POO/Serie.py
+++ Python/20230829_Avancando_POO/Serie.py
@@ -1,41 +1,20 @@
-class Serie:
+from ProgramaTV import ProgramaTV
+
+class Serie(ProgramaTV):
     # Declaração do construtor
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome
-        self.__ano = ano
+        super().__init__(nome, ano)
         self.__temporadas = temporadas
-        self.__likes = 0
 
     # Métodos públicos
-    # Método que adiciona um like
-    def darLike(self):
-        self.__likes += 1
 
     # Métodos getter e setter
     # Métodos getter
     @property
-    def nome(self):
-        return self.__nome
-    @property
-    def ano(self):
-        return self.__ano
-    @property
     def temporadas(self):
         return self.__temporadas
-    @property
-    def likes(self):
-        return self.__likes
     
     # Métodos setter
-    @nome.setter
-    def nome(self,valor):
-        self.__nome = valor
-    @ano.setter
-    def ano(self, valor):
-        self.__ano = valor
     @temporadas.setter
     def duracao(self, valor):
         self.__temporadas = valor
-    @likes.setter
-    def likes(self, valor):
-        self.__likes = valor
