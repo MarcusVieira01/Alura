87f9b8a25c855488688c8cb51d96c8d68a314056
Marcus Vieira <marcusvmvieira@gmail.com>
2023-09-01 20:16:08 -0300

    Implementação de dunder __getitem__ para usar duck typing

--- Python/20230829_Avancando_POO/Playlist.py
+++ Python/20230829_Avancando_POO/Playlist.py
@@ -1,19 +1,27 @@
-class Playlist(list):
+class Playlist:
     # Declaração de construtor
     def __init__(self, nome, listaProgramas):
-        super().__init__(listaProgramas)
         self.__nome = nome
+        self.__listaProgramas = listaProgramas
 
     # Declaração de métodos públicos
-    
+    # Declaração de método que forma um objeto iterável dentro da classe Playlist
+    def __getitem__(self, item):
+        return self.__listaProgramas[item]
     
     # Declaração de métodos getter e setter conforme a necessidade
     # Métodos getter
     @property
     def nome(self):
         return self.__nome
+    @property
+    def listaProgramas(self):
+        return self.__listaProgramas
+    @property
+    def tamanhoLista(self):
+        return len(self.__listaProgramas)
     
     # Métodos setter
     @nome.setter
-    def nome (self, valor):
+    def nome(self, valor):
         self.__nome = valor
