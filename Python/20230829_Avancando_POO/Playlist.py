01934fa77a9d3c1a4f608974a3155ddbcc862aa2
Marcus Vieira <marcusvmvieira@gmail.com>
2023-09-01 20:48:49 -0300

    Implementação do dunder method __len__ criando assim a propriedade sized para o atributo listaProgramas

--- Python/20230829_Avancando_POO/Playlist.py
+++ Python/20230829_Avancando_POO/Playlist.py
@@ -5,9 +5,12 @@ class Playlist:
         self.__listaProgramas = listaProgramas
 
     # Declaração de métodos públicos
-    # Declaração de método que forma um objeto iterável dentro da classe Playlist
+    # Declaração de método que tráz a propriedade iteravel para um atributo
     def __getitem__(self, item):
         return self.__listaProgramas[item]
+    # Declaração de método que retorna o tamanho de um atributo, deixando ele com a propriedade Sized
+    def __len__(self):
+        return len(self.__listaProgramas)
     
     # Declaração de métodos getter e setter conforme a necessidade
     # Métodos getter
