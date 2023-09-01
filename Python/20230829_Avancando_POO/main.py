5ccfce39a6ca668ed25fdae75adc411d8b4f042d
Marcus Vieira <marcusvmvieira@gmail.com>
2023-09-01 19:10:50 -0300

    Implementação de código teste

--- Python/20230829_Avancando_POO/main.py
+++ Python/20230829_Avancando_POO/main.py
@@ -7,21 +7,23 @@ atlanta = Serie("Atlanta", 2018, 2)
 tmep = Filme("Todo mundo em pânico", 1999, 180)
 demolidor = Serie("Demolidor", 2016, 8)
 
-listaProgramas = [vingadores, atlanta, tmep, demolidor]
+listaProg = [vingadores, atlanta, tmep, demolidor]
 
-playlistFDS = Playlist("Fim de semana", listaProgramas)
+playlistFDS = Playlist("Fim de semana", listaProg)
 
-for programas in playlistFDS.listaProgramas:
+print(f"O tamanho da play list é {len(playlistFDS)}")
+
+for programas in playlistFDS:
     print(programas.nome)
 
-for programas in playlistFDS.listaProgramas:
+for programas in playlistFDS:
     programas.darLike()
 
 tmep.darLike()
 
-for programas in playlistFDS.listaProgramas:
+for programas in playlistFDS:
     print(programas)
 
-for programas in playlistFDS.listaProgramas:
+for programas in playlistFDS:
     print(programas.__repr__())
 
