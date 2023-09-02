d3d2c5266669a0935ccca8141488f5ab9bfd8a30
Marcus Vieira <marcusvmvieira@gmail.com>
2023-09-02 14:08:45 -0300

    Atualização de arquivo com código teste

--- Python/20230901_String_Extrair_URL/main.py
+++ Python/20230901_String_Extrair_URL/main.py
@@ -3,6 +3,7 @@ from ExtratorURL import ExtratorURL
 url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100&taxa=0.12"
 
 urlExtraida = ExtratorURL(url)
+urlExtraida2 = ExtratorURL(url)
 
 print(urlExtraida.getURLBase())
 
@@ -11,3 +12,7 @@ print(urlExtraida.getURLParametros())
 print(urlExtraida.getValorParametro("quantidade"))
 
 print(f"A URL tem {len(urlExtraida)} caracteres")
+
+print(urlExtraida)
+
+print(urlExtraida == urlExtraida2)
