b4e09fc8a590410944d276c533973943852440dc
Marcus Vieira <marcusvmvieira@gmail.com>
2023-09-02 13:07:33 -0300

    Atualização do arquivo com teste de código

--- Python/20230901_String_Extrair_URL/main.py
+++ Python/20230901_String_Extrair_URL/main.py
@@ -1,8 +1,6 @@
 from ExtratorURL import ExtratorURL
 
-# url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100&taxa=0.12"
-
-url = None
+url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100&taxa=0.12"
 
 urlExtraida = ExtratorURL(url)
 
