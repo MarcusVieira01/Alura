569510a431bf73ee1afeb5a19540171846f92341
Marcus Vieira <marcusvmvieira@gmail.com>
2023-09-02 11:19:27 -0300

    Implementação de código teste

--- Python/20230901_String_Extrair_URL/main.py
+++ Python/20230901_String_Extrair_URL/main.py
@@ -1,24 +1,11 @@
-# url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100&taxa=0.12"
-url = "          "
+from ExtratorURL import ExtratorURL
 
-busca = "moedaOrigem"
+url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100&taxa=0.12"
 
-# Sanitização da URL
-url = url.replace(" ", "")
+urlExtraida = ExtratorURL(url)
 
-parametrosURL = url[url.find("?") + 1: len(url)]
+print(urlExtraida.getURLBase())
 
-arrayParametros = parametrosURL.split("&")
+print(urlExtraida.getURLParametros())
 
-if len(url) == 0:
-    raise ValueError("URL vazia")
-else:
-    print(f"Os parâmetros são {parametrosURL}")
-
-    for valor in arrayParametros:
-        elemento = valor.split("=")
-        if elemento [0] == busca:
-            print(f"O parâmetro buscado é {elemento[1]}")
-        else:
-            continue
-
+print(urlExtraida.getValorParametro(""))
