3711914f994dea595daede6086c45a9bbe9e8af0
Marcus Vieira <marcusvmvieira@gmail.com>
2023-09-02 20:45:38 -0300

    Atualização de arquivo com código teste

--- Python/20230902_Collections_List_Tupple/main.py
+++ Python/20230902_Collections_List_Tupple/main.py
@@ -6,9 +6,7 @@ conta16 = ContaCorrente(16)
 conta17 = ContaPoupanca(17)
 conta18 = ContaInvestimento(18)
 
-
 conta16.deposita(1000)
-
 conta17.deposita(2123)
 
 listaContas = [conta16, conta17, conta18]
