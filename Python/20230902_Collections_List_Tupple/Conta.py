de7009508ea667434cfce4f5eb24cbd2417194c4
Marcus Vieira <marcusvmvieira@gmail.com>
2023-09-02 17:46:51 -0300

    Criação de classe com abstração de uma Conta para ser herdada pelas subclasses conta corrente e conta poupança

---
+++ Python/20230902_Collections_List_Tupple/Conta.py
@@ -0,0 +1,29 @@
+class Conta:
+    ## Declaração de dunder methods
+    # Declaração de construtor
+    def __init__(self, codigo):
+        self.__codigo = codigo
+        self.__saldo = 0
+    # Declaração de método que fará a representação textual do objeto
+    def __str__(self):
+        return f'ContaCorrente (codigo={self.codigo}, saldo={self.__saldo})'
+    
+
+    ## Declaração de métodos getter e setter
+    # Getter
+    @property
+    def codigo(self):
+        return self.__codigo
+    @property
+    def saldo(self):
+        return self.__saldo
+    #Setter
+    @saldo.setter
+    def saldo(self, valor):
+        self.__saldo = valor
+    
+    ## Declaração de métodos públicos
+    # Declaração de método que fará a ação de depositar um valor na conta corrente
+    def deposita(self, valorDepositado):
+        self.saldo = self.saldo + valorDepositado
+        
