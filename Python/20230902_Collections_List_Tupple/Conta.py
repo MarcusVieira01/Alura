2a64f745c0f8db0b828efee76fe3c47d154788ab
Marcus Vieira <marcusvmvieira@gmail.com>
2023-09-02 20:58:01 -0300

    Implementação da anottation @total_ordering que fará a comparação total entre objetos

--- Python/20230902_Collections_List_Tupple/Conta.py
+++ Python/20230902_Collections_List_Tupple/Conta.py
@@ -1,5 +1,7 @@
 from abc import ABCMeta, abstractmethod
+from functools import total_ordering
 
+@total_ordering
 class Conta(metaclass=ABCMeta):
     ## Declaração de dunder methods
     # Declaração de construtor
