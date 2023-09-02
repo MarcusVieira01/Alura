c7d105e17542a2fffdffc265b240c685dcbd8af0
Marcus Vieira <marcusvmvieira@gmail.com>
2023-09-02 18:10:31 -0300

    Atualização de código teste

--- Python/20230902_Collections_List_Tupple/main.py
+++ Python/20230902_Collections_List_Tupple/main.py
@@ -1,14 +1,17 @@
 from ContaCorrente import ContaCorrente
 from ContaPoupanca import ContaPoupanca
+from ContaInvestimento import ContaInvestimento
 
 conta16 = ContaCorrente(16)
 conta17 = ContaPoupanca(17)
+conta18 = ContaInvestimento(18)
+
 
 conta16.deposita(1000)
 
 conta17.deposita(2123)
 
-listaContas = [conta16, conta17]
+listaContas = [conta16, conta17, conta18]
 
 for conta in listaContas:
     conta.passaMes()
