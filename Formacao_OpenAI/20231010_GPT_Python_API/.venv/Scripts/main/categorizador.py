06a3dd59884916c6109e59f5b5af3698a6f520bc
Marcus Vieira <marcusvmvieira@gmail.com>
2023-10-13 22:41:28 -0300

    Otimização do prompr de sistema utilizando prompt engineering e prompt template

--- Formacao_OpenAI/20231010_GPT_Python_API/.venv/Scripts/main/categorizador.py
+++ Formacao_OpenAI/20231010_GPT_Python_API/.venv/Scripts/main/categorizador.py
@@ -3,6 +3,47 @@ import openai
 import dotenv
 import os
 
+# Definição de função que categoriza produtos utilizando o modelo GPT 3.5 Turbo da OpenAI
+def categorizaProduto(nome_produto, categorias_validas):
+    # Atribuição de uma longstring à variável sendo o prompt de configuração do sistema
+    prompt_sistema = f""" 
+                        Você é um caracterizador de produtos. Usando somente a listagem abaixo de categorias válidas, categorize o produto inserido 
+                        ###Lista de categorias###
+                        {categorias_validas}
+                        ###Exemplo###
+                        Bola de tênis
+                        Esportes
+                      """
+
+    # Criação, usando o objeto ChatCompletion, de um chat com a IA da OpenAI
+    response = openai.ChatCompletion.create(
+        # Definição do modelo de IA a ser usado
+        model = "gpt-3.5-turbo",
+        # Definição de um array de dicionários com os parâmetros de configuração
+        messages = [
+            {
+                "role":"system",
+                "content":prompt_sistema
+            },
+            # Definição de dicionário para o prompt do usuário
+            {
+                "role":"user",
+                "content":nome_produto
+            }
+        ],
+        # Declaração dos parâmetros de configuração da resposta
+        temperature = 1,
+        max_tokens = 256,
+        top_p = 1,
+        frequency_penalty = 0,
+        presence_penalty = 0
+    )
+
+    print(response.choices[0].message.content)
+
+    print(f"Tokens usados:{response.usage.total_tokens}")
+
+
 # Chamada da função load_dotenv() para carga das variáveis de ambiente
 dotenv.load_dotenv()
 
@@ -11,46 +52,17 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 # Chamada de atributo e atribuição do valor da ORGANIZATION cadastrada dentro do OpenAI
 openai.organization = os.getenv("ORGANIZATION")
 
-# Atribuição de uma longstring à variável sendo o prompt de configuração do sistema
-prompt_sistema = """ 
-                    Você é um caracterizador de produtos, onde deve escolher uma categoria abaixo:
-                    ###Lista de categorias###
-                    1. Beleza
-                    2. Entreterimento
-                    3. Esportes
-                    4. Outros
-                    ###Exemplo###
-                    Bola de tênis
-                    Esportes
-                 """
+# Imprime mensagem e captura input do usuário, atribuindo o valor à variável com as categorias válidas
+print("Digite as categorias válidas:")
+categorias_validas = input()
 
-# Criação, usando o objeto ChatCompletion, de um chat com a IA da OpenAI
-response = openai.ChatCompletion.create(
-    # Definição do modelo de IA a ser usado
-    model = "gpt-3.5-turbo",
-    # Definição de um array de dicionários com os parâmetros de configuração
-    messages = [
-        {
-            "role":"system",
-            "content":prompt_sistema
-        },
-        # Definição de dicionário para o prompt do usuário
-        {
-            "role":"user",
-            "content":"Tablet"
-        }
-    ],
-    # Declaração dos parâmetros de configuração da resposta
-    temperature = 1,
-    max_tokens = 256,
-    top_p = 1,
-    frequency_penalty = 0,
-    presence_penalty = 0,
-    n = 5
-)
+# Loop while com condição de parada True, gerando um loop infinito com a evocação da função de categorização de produtos
+while(True):
+    # Imprime mensagem e captura input do usuário, atribuindo o valor à variável com o nome do produto à ser categorizado
+    print("Digite o nome do produto:")
+    nome_produto = input()
 
-for i in range(0, 5):
-    print(response.choices[i].message.content)
-    print("-------")
+    # Evocação de função que categorizará os produtos inputados pelo usuário
+    categorizaProduto(nome_produto, categorias_validas)
 
-print(f"Tokens usados:{response.usage.total_tokens}")
+
