fbb7bd556454328838d130db2a523449566ca12e
Marcus Vieira <marcusvmvieira@gmail.com>
2023-10-15 16:39:39 -0300

    Implementação de biblioteca validate-docbr, usando a classe CPF e refatoração do código de validação e dunder method __str__

--- Python/20230907_ValidacaoDados/Cpf.py
+++ Python/20230907_ValidacaoDados/Cpf.py
@@ -1,3 +1,6 @@
+# Importaçãod e bibliotecas e calsses
+from validate_docbr import CPF
+
 # Declaração de classe que abstrai o conceito de CPF
 class Cpf:
     # Definição de dunder method que fará a construção do objeto
@@ -7,30 +10,24 @@ class Cpf:
         # Condicional que valida a quantidade de caracteres do atributo cpf, lançando exceção de ValueError
         if self.__validaCPF(cpf):
             self.__cpf = cpf
-            self.__validado = self.__validaCPF(cpf)
         else:
             raise ValueError("CPF inválido!")
 
     # Definição de dunder method que retornará a representação de caracteres do objeto, no caso o valor do 
     # atributo cpf formatado para a representação pt-br
     def __str__(self):
-        return self.__mascaraCPF()
+        #
+        mascaraCPF = CPF()
+        return mascaraCPF.mask(self.__cpf)
 
     # Definição de método auxiliar que validará a quantidade de caracteres do atributo cpf
     def __validaCPF(self, cpf):
         # Condicional, fail fast, que verificará se o valor do atributo cpf possui 11 elementos e retornará 
         # True caso tenha e False caso não
         if len(cpf) != 11:
-            return False
+            raise ValueError("Quantidade de dígitos inválida!")
         else:
-            return True
-    
-    # Definição de método que fará a formatação do valor da variável cpf conforme o pardão pr-br
-    def __mascaraCPF(self):
-        # Retorno do valor do atributo cpf no formato pr-br
-        return (f"{self.__cpf[0:3]}.{self.__cpf[3:6]}.{self.__cpf[6:9]}-{self.__cpf[9:]}")
-    
-    # Métodos getters
-    # Método getter para o atributo privado __validado
-    def validado(self):
-        return self.__validado
+            # Instanciação de objeto da classe CPF
+            validador = CPF()
+            #
+            return validador.validate(cpf)
