3bc5d64d3a5a2517652c79a0e081fa05d9ffa0c4
Marcus Vieira <marcusvmvieira@gmail.com>
2023-10-15 20:21:39 -0300

    Implementação de código com um objeto funcionário e o valor de retorno da função idade()

---
+++ Python/20231015_TDD_Python/codigo/main.py
@@ -0,0 +1,8 @@
+# Importação de módulos ou classes
+from bytebank import Funcionario
+
+# Instanciamento de objeto do tipo Funcionário via construtor
+funcionario = Funcionario("Lucas", 1985, 4300.00)
+
+# Exibição do retorno do método idade
+print(funcionario.idade())
