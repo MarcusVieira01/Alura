fa4131f6ab715a1c47f5e7db68ce5cf57d2f0833
Marcus Vieira <marcusvmvieira@gmail.com>
2023-10-15 16:06:48 -0300

    Implementação de biblioteca Cpf, dotenv e os para uso e testes

---
+++ Python/20230907_ValidacaoDados/main.py
@@ -0,0 +1,14 @@
+# Importação de bibliotecas e classes
+from Cpf import Cpf
+import dotenv
+import os
+
+#
+dotenv.load_dotenv()
+#
+cpf = os.getenv("CPF_REAL")
+
+objCPF = Cpf(cpf)
+
+print(objCPF)
+print(objCPF.validado())
