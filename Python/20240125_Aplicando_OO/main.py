a9b43ac2ab0ba830c836820042c9e56b3df5496c
Marcus Vieira <marcusvmvieira@gmail.com>
2024-01-25 14:55:49 -0300

    Refatoração para continuidade das aulas

--- Python/20240125_Aplicando_OO/main.py
+++ Python/20240125_Aplicando_OO/main.py
@@ -1,7 +1,15 @@
 from models.Restaurante import Restaurante
 
-restaurante_praca = Restaurante('praça', "gourmet")
-restaurante_pizza = Restaurante('pizza express', "fastfood")
+def main():
+    restaurante_praca = Restaurante('praça'        , "gourmet" )
+    restaurante_pizza = Restaurante('pizza express', "fastfood")
 
-print(Restaurante.listar_restaurantes(Restaurante))
+    print(Restaurante.listar_restaurantes())
 
+    restaurante_praca.alternar_ativo()
+    restaurante_pizza.alternar_ativo()
+
+    print(Restaurante.listar_restaurantes())
+
+if __name__ == '__main__':
+    main()
