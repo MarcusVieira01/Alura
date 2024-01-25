0bc4f00b250a3e3f4d2f9df05a697ea7848a4b9d
Marcus Vieira <marcusvmvieira@gmail.com>
2024-01-25 14:56:20 -0300

    Refatoração da classe para que seja inclusa as avaliações via classe Avaliacao.py

--- Python/20240125_Aplicando_OO/models/Restaurante.py
+++ Python/20240125_Aplicando_OO/models/Restaurante.py
@@ -1,3 +1,5 @@
+from models.Avaliacao import Avaliacao
+
 # Classe que instancia objetos do tipo Restaurante
 class Restaurante:
     # Declaração de atributo da classe, sem autoreferência
@@ -6,12 +8,13 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self._nome = nome.title()
         self._categoria = categoria.upper()
+        self._avaliacao = []
         self._ativo = False
         # Método sem autoreferência e evocado pelo construtor
         Restaurante.restaurantes.append(self)
     def __str__(self):
-        return f'Nome: {self._nome} | Categoria: {self._categoria} | Ativo: {self._ativo}'
-    
+        return f'Nome: {self._nome} | Categoria: {self._categoria} | Avaliação: {self._avaliacao} | Ativo: {self._ativo}'
+
     # Métodos getter
     @property
     def nome(self):
@@ -21,13 +24,19 @@ class Restaurante:
         return self._categoria
     @property
     def ativo(self):
-        return 'Verdadeiro!' if self._ativo else 'Falso'
+        return 'Verdadeiro' if self._ativo else 'Falso'
+    
     # Métodos setter
-    
-    
-    # Métodos individuais
-    def listar_restaurantes(self):
-        print(Restaurante.restaurantes)
+    def alternar_ativo(self):
+        self._ativo = not self._ativo
+    def receber_avaliacao(self, cliente, nota):
+        # Instancia de Avaliação
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+        
+    # Método da classe
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{"Nome do Restaurante".ljust(20)} | {"Categoria".ljust(20)} | {"Ativo"}')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante.nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo.ljust(20)}')
