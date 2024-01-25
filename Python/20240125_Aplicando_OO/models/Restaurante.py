602909cd897e658091eba8feefb5c5c624759835
Marcus Vieira <marcusvmvieira@gmail.com>
2024-01-25 14:22:17 -0300

    Refatoração com implementação de atributos protegidos e métodos getter, além de refatoração dos métodos que acessam estes atributos

--- Python/20240125_Aplicando_OO/models/Restaurante.py
+++ Python/20240125_Aplicando_OO/models/Restaurante.py
@@ -1,9 +1,33 @@
 # Classe que instancia objetos do tipo Restaurante
 class Restaurante:
-    def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
-    def __str__(self):
-        return f"Nome: {self.nome} | Categoria: {self.categoria}"
+    # Declaração de atributo da classe, sem autoreferência
+    restaurantes = []
 
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        # Método sem autoreferência e evocado pelo construtor
+        Restaurante.restaurantes.append(self)
+    def __str__(self):
+        return f'Nome: {self._nome} | Categoria: {self._categoria} | Ativo: {self._ativo}'
+    
+    # Métodos getter
+    @property
+    def nome(self):
+        return self._nome
+    @property
+    def categoria(self):
+        return self._categoria
+    @property
+    def ativo(self):
+        return 'Verdadeiro!' if self._ativo else 'Falso'
+    # Métodos setter
+    
+    
+    # Métodos individuais
+    def listar_restaurantes(self):
+        print(Restaurante.restaurantes)
+        print(f'{"Nome do Restaurante".ljust(20)} | {"Categoria".ljust(20)} | {"Ativo"}')
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo.ljust(20)}')
