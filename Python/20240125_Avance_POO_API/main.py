ac753d3e7cb9bd0f340e05133f64e713af90e021
Marcus Vieira <marcusvmvieira@gmail.com>
2024-01-26 20:32:24 -0300

    Implementação de método adiciona_no_cardápio() e exibe_cardapio()

--- Python/20240125_Avance_POO_API/main.py
+++ Python/20240125_Avance_POO_API/main.py
@@ -2,13 +2,16 @@ from models.restaurante import Restaurante
 from models.cardapio.bebida import Bebida
 from models.cardapio.prato import Prato
 
-restaurante_praca = Restaurante('praça gourmet', 'gourmet')
 bebida_suco       = Bebida('Melancia', 2.45, '350ml')
 prato_paozinho    = Prato('Paozinho', 2.00, 'Paozinho francês')
 
+restaurante_praca = Restaurante('praça gourmet', 'gourmet')
+
+restaurante_praca.adiciona_no_cardapio(bebida_suco)
+restaurante_praca.adiciona_no_cardapio(prato_paozinho)
+
 def main():
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibe_cardapio()
 
 if __name__ == '__main__':
     main()
