ff6e98ea274f72623adee164b26e1a2eb700337b
Marcus Vieira <marcusvmvieira@gmail.com>
2024-01-26 19:40:23 -0300

    Implementação de script para instanciação de objetos e continuidade da aula

--- Python/20240125_Avance_POO_API/main.py
+++ Python/20240125_Avance_POO_API/main.py
@@ -1,9 +1,14 @@
-from.models.restaurante import Restaurante
+from models.restaurante import Restaurante
+from models.cardapio.bebida import Bebida
+from models.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça gourmet', 'gourmet')
+bebida_suco       = Bebida('Melancia', 2.45, '350ml')
+prato_paozinho    = Prato('Paozinho', 2.00, 'Paozinho francês')
 
 def main():
-    pass
+    print(bebida_suco)
+    print(prato_paozinho)
 
 if __name__ == '__main__':
     main()
