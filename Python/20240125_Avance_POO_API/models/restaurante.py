326dd68b148f88ea6e83f523997258ae070e72a5
Marcus Vieira <marcusvmvieira@gmail.com>
2024-01-26 20:33:57 -0300

    Implementação de métodos adiciona_no_cardápio() e exibe_cardapio() na classe

--- Python/20240125_Avance_POO_API/models/restaurante.py
+++ Python/20240125_Avance_POO_API/models/restaurante.py
@@ -1,5 +1,6 @@
 '''Importação de classe externa'''
 from models.avaliacao import Avaliacao
+from models.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     '''Classe que abstrai o conceito de restaurante'''
@@ -10,6 +11,7 @@ class Restaurante:
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._avaliacao = []
+        self._cardapio = []
         self._ativo = False
         # Método sem autoreferência e evocado pelo construtor
         Restaurante.restaurantes.append(self)
@@ -30,13 +32,14 @@ class Restaurante:
     def ativo(self):
         '''Método getter para o atributo ativo, após validação'''
         return 'Verdadeiro' if self._ativo else 'Falso'
-    
+
     # Métodos setter
     def alternar_ativo(self):
         '''Método setter para o atributo ativo'''
         self._ativo = not self._ativo
     def receber_avaliacao(self, cliente, nota):
-        '''Método setter para o atributo avaliacao, que fará validação do valor da nota limitando-a em 0 ou 5'''
+        '''Método setter para o atributo avaliacao, que fará validação do valor da nota limitando-a 
+        em 0 ou 5'''
         if nota > 5:
             nota = 5
         elif nota < 0:
@@ -47,18 +50,35 @@ class Restaurante:
     # Método da classe
     @classmethod
     def listar_restaurantes(cls):
-        '''Imprime a lista de restaurantes com seu atributos formatados, onde o atributo Avaliação será a média das 
-        avaliações recebidas'''
+        '''Imprime a lista de restaurantes com seu atributos formatados, onde o atributo Avaliação 
+        será a média das avaliações recebidas'''
         print(f'{"Nome do Restaurante".ljust(20)} | {"Categoria".ljust(20)} | {"Avaliação".ljust(20)} | {"Ativo"}')
         for restaurante in cls.restaurantes:
             print(f'{restaurante.nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {str(restaurante.media_avaliacoes).ljust(20)} | {restaurante.ativo.ljust(20)}')
-    
+
     # Métodos próprios
     @property
     def media_avaliacoes(self):
-        '''Método que realiza validação se o restaurante possui avaliações, retornando uma string caso não possua e a 
-        média arredondada para uma casa decimal caso possua'''
+        '''Método que realiza validação se o restaurante possui avaliações, retornando uma string 
+        caso não possua e a média arredondada para uma casa decimal caso possua'''
         if not self._avaliacao:
             return 'Sem Avaliações'
         else:
             return round(sum(avaliacao.nota for avaliacao in self._avaliacao) / len(self._avaliacao), 1)
+
+    def adiciona_no_cardapio(self, item):
+        '''Função que faz o append do objeto item ao atributo cardápio, após validação de 
+        instância'''
+        if isinstance(item, ItemCardapio):
+            self._cardapio.append(item)
+
+    def exibe_cardapio(self):
+        print(f'Cardápio do restaurante {self._nome}:')
+        print(f'{"Nome".ljust(22)} | {"Descrição/Tamanho".ljust(20)} | {"Preço"}')
+        for indice, item in enumerate(self._cardapio, start=1):
+            if hasattr(item, 'descricao'):
+                print(f'{indice}.{item.nome.ljust(20)} | {item.descricao.ljust(20)} | R${item.preco:.2f}')
+            elif hasattr(item, 'tamanho'):
+                print(f'{indice}.{item.nome.ljust(20)} | {item.tamanho.ljust(20)} | R${item.preco:.2f}')
+            else:
+                print('Sem itens no cardápio!')
