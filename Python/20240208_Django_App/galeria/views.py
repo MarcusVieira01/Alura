a94f85504b1b48727bb1cef763697d8c5c882b86
Marcus Vieira <marcusvmvieira@gmail.com>
2024-02-13 16:22:08 -0300

    Refatoração do arquivo para inclusão de dicionário e inclusão como argumento para o método render(args)

--- Python/20240208_Django_App/galeria/views.py
+++ Python/20240208_Django_App/galeria/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+# Definição de rotas
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados ={
+        1: {"nome"   :"Nebulosa de Carine",
+            "legenda": "webbtelescope.org / NASA / James Web Space Telescope"},
+        2: {"nome"   :"Galáxia NGC179",
+            "legenda": "nasa.org / NASA / Hubble Space Telescope"},
+    }
+    return render(request, 'galeria/index.html', {'cards': dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
+
+# Definição de funções
